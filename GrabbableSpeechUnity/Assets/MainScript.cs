744f1b7af04fc51f05f096fc02702796bf7e6199
derp derp <workingbahm@yahoo.com.sg>
2019-07-21 16:16:59 -0700

    Ouch

--- GrabbableSpeechUnity/Assets/MainScript.cs
+++ GrabbableSpeechUnity/Assets/MainScript.cs
@@ -2,7 +2,40 @@
 using UnityEngine.UI;
 using Microsoft.CognitiveServices.Speech;
 using TMPro;
+using System.Text;
+using System.Collections.Generic;
+using System;
 
 public class MainScript : MonoBehaviour
 {
+    public Color InProgressTextColor;
+    public TextMeshProUGUI OutputTextObj;
+
+    public static MainScript Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        OutputTextObj.text = GetFormattedLogText();
+    }
+
+    private string GetFormattedLogText()
+    {
+        string colorString = GetHexString(MainScript.Instance.InProgressTextColor);
+        string speechInProgess = SpeechRecognitionManager.Instance.SpeechInProgress;
+        string inProgressPart = "<color=" + colorString + ">" + speechInProgess + "</color>";
+        return SpeechRecognitionManager.Instance.Log + inProgressPart;
+    }
+
+    private static string GetHexString(Color color)
+    {
+        byte r = (byte)(byte.MaxValue * color.r);
+        byte g = (byte)(byte.MaxValue * color.g);
+        byte b = (byte)(byte.MaxValue * color.b);
+        return "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+    }
 }
--- GrabbableSpeechUnity/Assets/SpeechRecognitionManager.cs
+++ GrabbableSpeechUnity/Assets/SpeechRecognitionManager.cs
@@ -2,61 +2,68 @@
 using Microsoft.CognitiveServices.Speech;
 using TMPro;
 using System.Text;
+using System.Collections.Generic;
+using System;
 
 public class SpeechRecognitionManager : MonoBehaviour
 {
-    public Color InProgressTextColor;
-    public TextMeshPro outputText;
-    private StringBuilder messageLog = new StringBuilder();
-    private string currentMessage;
+    private readonly List<SpeechBlock> speechBlocks = new List<SpeechBlock>();
+    public string SpeechInProgress { get; private set; }
+    public string Log { get; private set; }
 
     private SpeechRecognizer recognizer;
-    private object threadLocker = new object();
+    private readonly object threadLocker = new object();
+
+    public static SpeechRecognitionManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
+        StartRecognizer();
+    }
+
+    private void StartRecognizer()
+    {
         SpeechConfig config = SpeechConfig.FromSubscription(SpeechSubscriptionKey.Key, "westus");
         recognizer = new SpeechRecognizer(config);
-        recognizer.SpeechStartDetected += Recognizer_SpeechStartDetected;
         recognizer.Recognizing += Recognizer_Recognizing;
-        recognizer.Recognized += Recognizer_Recognized;
+        //recognizer.Recognized += Recognizer_Recognized;
         recognizer.StartContinuousRecognitionAsync();
-    }
-
-    private static string GetHexString(Color color)
-    {
-        byte r = (byte)(byte.MaxValue * color.r);
-        byte g = (byte)(byte.MaxValue * color.g);
-        byte b = (byte)(byte.MaxValue * color.b);
-        return "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
-    }
-
-    void Update()
-    {
-        string colorString = GetHexString(InProgressTextColor);
-        string inProgressPart = "<color=" + colorString + ">" + currentMessage + "</color>";
-        outputText.text = messageLog.ToString() + inProgressPart;
     }
 
     private void Recognizer_Recognized(object sender, SpeechRecognitionEventArgs e)
     {
+        Debug.Log(e);
+        SpeechBlock newBlock = new SpeechBlock(e.Result.Text, DateTime.Now);
         lock (threadLocker)
         {
-            currentMessage = "";
-            messageLog.AppendLine(e.Result.Text);
+            SpeechInProgress = "";
+            speechBlocks.Add(newBlock);
+            Log = UpdateLog();
         }
+    }
+
+    private string UpdateLog()
+    {
+        StringBuilder logBuilder = new StringBuilder();
+        foreach (SpeechBlock item in speechBlocks)
+        {
+            logBuilder.AppendLine(item.Text);
+        }
+        return logBuilder.ToString();
     }
 
     private void Recognizer_Recognizing(object sender, SpeechRecognitionEventArgs e)
     {
         lock (threadLocker)
         {
-            currentMessage = e.Result.Text;
+            Debug.Log(e);
+            //SpeechInProgress = e.Result.Text;
         }
-    }
-
-    private void Recognizer_SpeechStartDetected(object sender, RecognitionEventArgs e)
-    {
     }
 
     private void OnDestroy()
@@ -65,5 +72,17 @@ public class SpeechRecognitionManager : MonoBehaviour
         {
             recognizer.Dispose();
         }
+    }
+}
+
+public class SpeechBlock
+{
+    public DateTime TimeStamp { get; }
+    public string Text { get; }
+
+    public SpeechBlock(string text, DateTime timeStamp)
+    {
+        Text = text;
+        TimeStamp = timeStamp;
     }
 }
